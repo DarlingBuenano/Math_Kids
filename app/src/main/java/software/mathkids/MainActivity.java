1de39f937e4bbdf245d835b61f5b1d7fc8d69534
DarlynYami05 <dwverling@gmail.com>
2021-08-14 15:25:20 -0500

    Pantalla de Sumar/Contar terminada

--- app/src/main/java/software/mathkids/MainActivity.java
+++ app/src/main/java/software/mathkids/MainActivity.java
@@ -3,6 +3,7 @@ package software.mathkids;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    public void ontouchTocarPantalla(View view){
+
     }
 }
