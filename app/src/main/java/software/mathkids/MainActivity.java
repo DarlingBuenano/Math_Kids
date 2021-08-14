8c56f763944914726abb500c27c294ee83d1d050
DarlynYami05 <dwverling@gmail.com>
2021-08-14 18:56:27 -0500

    Pantalla de configurando los eventos touch

--- app/src/main/java/software/mathkids/MainActivity.java
+++ app/src/main/java/software/mathkids/MainActivity.java
@@ -2,8 +2,10 @@ package software.mathkids;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ontouchTocarPantalla(View view){
+        Toast.makeText(getApplicationContext(), "Tocaste la pantalla", Toast.LENGTH_SHORT).show();
 
+        //Aqui programar para ir a la siguiente pantalla
+        Intent puente = new Intent(this.getApplicationContext(), OpcionesActivity.class);
+        startActivity(puente);
     }
 }
